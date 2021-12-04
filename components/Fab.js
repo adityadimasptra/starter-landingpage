bc5febf4b7444c98262c13824f28e4a0cdec8d37
Aditya Dimas Saputra <aditya@investree.id>
2021-12-04 13:23:24 +0700

    [ADITYA] Fix property class

--- components/Fab.js
+++ components/Fab.js
@@ -6,9 +6,9 @@ const FloatingButton = () => {
         <a className="" href="https://wa.me/15551234567" target="_blank" rel="noreferrer">
           <img className="h-16" src="/whatsapp-fab.svg" alt="call-whatsapp" />
         </a>
-        <span class="flex absolute top-0 right-0 h-3 w-3">
-          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-          <span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+        <span className="flex absolute top-0 right-0 h-3 w-3">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
         </span>
     </div>
    );
