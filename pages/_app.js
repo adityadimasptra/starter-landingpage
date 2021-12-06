2aefbe1c541211c65b853cd3238f6fc7825563c7
liddd <khalid@investree.id>
2021-12-06 23:39:19 +0700

    [KHALID] add cropped logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
-import Layout from '../components/Layout'
-import Head from 'next/head';
-import 'tailwindcss/tailwind.css'
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <link rel="shortcut icon" href="/treescoco_logo.png" />
+        <link rel="shortcut icon" href="/treescoco_logo_crop.png" />
       </Head>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
