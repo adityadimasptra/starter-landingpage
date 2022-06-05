b388b81c51b2f4ecf2b650eedb6765782a508d56
Aditya Dimas Saputra <aditya@investree.id>
2022-06-06 00:22:41 +0700

    [ADITYA] wa number at fab

--- components/Fab.js
+++ components/Fab.js
@@ -1,7 +1,7 @@
 const FloatingButton = () => {
   return ( 
     <div className="fab fixed right-5 z-20 bottom-5 rounded-full">
-        <a className="" href="https://wa.me/15551234567" target="_blank" rel="noreferrer">
+        <a className="" href="https://wa.me/6281332693518" target="_blank" rel="noreferrer">
           <img className="h-16" src="/whatsapp-fab.svg" alt="call-whatsapp" />
         </a>
         <span className="flex absolute top-0 right-0 h-3 w-3">
